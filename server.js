8a1e32289899a749e1aa5952ed7000cfe0db371d
Leo Esc <leonardodanielescoto@gmail.com>
2018-04-07 19:14:25 -0500

    Use port 80

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 console.log("< Server >");
 const fs = require("fs-extra");
 const {serve, html} = require("servecube");
-const youKnow = require("./data/youknow.js");
+const secret = require("./data/secret.js");
 const production = process.argv[2] === "production";
 (async () => {
 	const cube = await serve({
 		eval: v => {
 			return eval(v);
 		},
-		domain: "comedy-dot.gold",
-		httpPort: 8080,
-		httpsRedirect: production,
+		domain: production ? "comedy-dot.gold" : "localhost",
+		httpPort: 80,
 		githubPayloadURL: "/githubwebhook",
-		githubSecret: youKnow.github.secret,
-		githubToken: youKnow.github.token
+		githubSecret: secret.github.secret,
+		githubToken: secret.github.token
 	});
 	const {load} = cube;
 	process.openStdin().on("data", input => {
