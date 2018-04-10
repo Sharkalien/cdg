a6be30e48b65c5b7d8770d2f1c6c4d4d2a897381
Grant Gryczan <grantgryczan@gmail.com>
2018-04-09 22:47:07 -0400

    ServeCube 1.13.1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const production = process.argv[2] === "production";
 (async () => {
 	const cube = await serve({
 		eval: v => eval(v),
-		domain: production ? "comedy-dot.gold" : "localhost",
+		domain: production ? "comedy-dot.gold" : "localhost:8080",
 		errorDir: "error",
 		httpPort: 8080,
 		githubPayloadURL: "/githubwebhook",
