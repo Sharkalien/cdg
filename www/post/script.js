ca4556f70f08efa1b88403b0587135401633d08b
Grant Gryczan <grantgryczan@gmail.com>
2018-07-29 05:14:20 -0400

    Prevent default form submission

--- www/post/script.js
+++ www/post/script.js
@@ -1,4 +1,5 @@
 document.querySelector("form").addEventListener("submit", evt => {
+	evt.preventDefault();
 	gapi.load("auth2", () => {
 		gapi.auth2.init({
 			client_id: "148379982026-77jpbdhuohqvvar9bn2lohk4uct6aauo.apps.googleusercontent.com"
