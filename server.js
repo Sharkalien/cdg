de3116276cd0f654444fc83c6747f65874fb52e8
Grant Gryczan <grantgryczan@gmail.com>
2018-08-01 02:43:45 -0400

    Render individual posts

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const pageNameTest = /\/(.*?)\/?$/;
 const brs = /\n/g;
 const whitespace = /[\s-]+/g;
 const hyphens = /-/g;
+const postTagTest = /^post-(\d)+$/;
 const formatDate = d => {
 	d = new Date(d);
 	return `${d.getMonth() + 1}-${d.getDate()}.${d.getFullYear()}`;
@@ -16,46 +17,56 @@ const formatDate = d => {
 const cleanTag = tag => tag && tag.trim().toLowerCase().replace(whitespace, "-");
 const forTags = (tag, i, tags) => tag && tags.indexOf(tag) === i;
 const byTagHTML = tag => html`<a class="tag" href="/tagged/$${tag}">$${tag.replace(hyphens, " ")}</a>`;
+const byTagClass = tag => `tag_${tag}`;
 const postsPerPage = 10;
 (async () => {
 	const myEval = v => eval(v);
 	require("replthis")(myEval);
 	const users = JSON.parse(await fs.readFile("secret/users.json"));
 	const posts = JSON.parse(await fs.readFile("secret/posts.json"));
+	const renderPost = (id, i) => html`
+		<div id="post_${id}" class="post box user_$${users[posts[i].user].name} $${posts[i].tags.map(byTagClass).join(" ")}">
+			<div class="header">
+				By <a class="author" href="/tagged/$${users[posts[i].user].name}" style="$${users[posts[i].user].style}">$${users[posts[i].user].name}</a>
+			</div>
+			<div class="body">${posts[i].body}</div>
+			<div class="footer">
+				<a class="date" href="/tagged/post-${id}">${formatDate(posts[i].date)}</a>
+				<span class="tags">${posts[i].tags.map(byTagHTML).join(", ")}</span>
+			</div>
+		</div>
+	`;
+	const noPosts = html`<br><center>THE COMEDY GOLD MINE HAS RUN DRY.</center>`;
 	const renderPosts = (page, tag) => {
 		let value = tag ? html`<br><i>tagged: $${tag}</i><br>` : "";
 		tag = cleanTag(tag);
-		const targetPosts = tag ? posts.filter(post => cleanTag(users[post.user].name) === tag || post.tags.includes(tag)) : posts;
-		if(targetPosts.length) {
-			const maxPage = Math.ceil(targetPosts.length / postsPerPage);
-			page = Math.min(maxPage, Math.ceil(page));
-			let start = (postsPerPage * (page - 2) + targetPosts.length % postsPerPage) % -10;
-			const end = Math.min(targetPosts.length, start + postsPerPage - 1);
-			start = Math.max(0, start);
-			for(let i = end; i >= start; i--) {
-				value += html`
-					<div id="post_${posts.indexOf(targetPosts[i]) + 1}" class="post box post_$${users[targetPosts[i].user].name}">
-						<div class="header">
-							By <a class="author" href="/tagged/$${users[targetPosts[i].user].name}" style="$${users[targetPosts[i].user].style}">$${users[targetPosts[i].user].name}</a>
-						</div>
-						<div class="body">${targetPosts[i].body}</div>
-						<div class="footer">
-							<span class="date">${formatDate(targetPosts[i].date)}</span>
-							<span class="tags">${targetPosts[i].tags.map(byTagHTML).join(", ")}</span>
-						</div>
+		if(postTagTest.test(tag)) {
+			const id = tag.replace(postTagTest, "$1");
+			const i = id - 1;
+			return value + (posts[i] ? renderPost(id, i) : noPosts);
+		} else {
+			const targetPosts = tag ? posts.filter(post => cleanTag(users[post.user].name) === tag || post.tags.includes(tag)) : posts;
+			if(targetPosts.length) {
+				const maxPage = Math.ceil(targetPosts.length / postsPerPage);
+				page = Math.min(maxPage, Math.ceil(page));
+				let start = (postsPerPage * (page - 2) + targetPosts.length % postsPerPage) % -10;
+				const end = Math.min(targetPosts.length, start + postsPerPage - 1);
+				start = Math.max(0, start);
+				for(let i = end; i >= start; i--) {
+					const index = posts.indexOf(targetPosts[i]);
+					value += renderPost(index + 1, index);
+				}
+				const urlStart = tag ? html`/tagged/$${tag}/` : "/page/";
+				const showPrevButton = page > 1;
+				const showNextButton = page < maxPage;
+				return value + html`
+					<div id="buttons">
+						${(showPrevButton ? html`<a href="${urlStart}1"><img src="/img/arrow_first.png"></a>&nbsp;<a href="${urlStart + (page - 1)}"><img src="/img/arrow_prev.png"></a>` : "") + (showPrevButton && showNextButton ? html`&nbsp;<img src="/img/arrow_dot.png">&nbsp;` : "") + (showNextButton ? html`<a href="${urlStart + (page + 1)}"><img src="/img/arrow_next.png"></a>&nbsp;<a href="${urlStart + maxPage}"><img src="/img/arrow_last.png"></a>` : "")}
 					</div>
 				`;
+			} else {
+				return value + noPosts;
 			}
-			const urlStart = tag ? html`/tagged/$${tag}/` : "/page/";
-			const showPrevButton = page > 1;
-			const showNextButton = page < maxPage;
-			return value + html`
-				<div id="buttons">
-					${(showPrevButton ? html`<a href="${urlStart}1"><img src="/img/arrow_first.png"></a>&nbsp;<a href="${urlStart + (page - 1)}"><img src="/img/arrow_prev.png"></a>` : "") + (showPrevButton && showNextButton ? html`&nbsp;<img src="/img/arrow_dot.png">&nbsp;` : "") + (showNextButton ? html`<a href="${urlStart + (page + 1)}"><img src="/img/arrow_next.png"></a>&nbsp;<a href="${urlStart + maxPage}"><img src="/img/arrow_last.png"></a>` : "")}
-				</div>
-			`;
-		} else {
-			return value + html`<br><center>THE COMEDY GOLD MINE HAS RUN DRY.</center>`;
 		}
 	};
 	const verify = context => new Promise(resolve => {
